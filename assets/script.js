61bedd8f029574bb619b1324060f391e71bfdc6f
unknown <whritenourc@outlook.com>
2024-01-07 20:05:09 -0600

    the quiz returns a logical response

--- assets/script.js
+++ assets/script.js
@@ -27,141 +27,80 @@ var answer4El = document.querySelector("#answer4");
 
 /* Food Variables */
 
-const mexicanFoods = {
-    heavy: ['Burrito', 'Tacos', 'Enchiladas'],
-    light: ['Nachos', 'Quesadilla', 'Fajitas']
-};
-
-const indianFoods = {
-    heavy: ['butter-chicken', 'chicken-masala', 'tikka', 'biryani', 'pakora'],
-    light: ['naan', 'samosa']
-}
-
-const thaiFoods = {
-    heavy: ['pad-thai', 'khao-pad', 'pad-kra-pao-moo'],
-    light: ['panang-curry', 'tom-yum-goong', 'tom-kha-gai']
-}
-
-const americanFoods = {
-    heavy: ['burgers', 'steak', 'wings', 'fried-chicken', 'french-fries'],
-    light: ['salad', 'hot-dog']
-}
-
-const italianFoods = {
-    heavy: ['spaghetti', 'pizza', 'lasagna', 'chicken-parm'],
-    light: ['bruscetta', 'risotto']
-}
-
-const jewishFoods = {
-    heavy: ['knish', 'deli-sammy', 'challah', 'latkes'],
-    light: ['matzo-ball-soup', 'creamcheese-lox']
-}
-
-const chineseFoods = {
-    heavy: ['orange-chicken', 'mongolian-beef', 'kung-pao-shrimp'],
-    light: ['eggroll', 'lo-mein', 'fried-rice']
-}
-
-const vietnameseFoods = {
-    heavy: ['bahn-mi', 'com-tam', 'bun-cha', 'goi-cuon'],
-    light: ['spring-roll', 'pho']
-}
-
-const japaneseFoods = {
-    heavy: ['ramen', 'teriyaki', 'tempura', 'katsudon'],
-    light: ['sushi', 'takoyaki']
-}
-
-const iceCreamFoods = {
-    heavy: ['ice-cream-cake', 'banana-split', 'milkshake'],
-    light: ['ice-cream-cone', 'sundae', 'mochi']
-}
-
-const bakeryFoods = {
-    heavy: ['cake', 'pie', 'brownies'],
-    light: ['crossiant', 'cookies', 'donut']
-}
-
-const frenchFoods = {
-    heavy: [],
-    light: []
-}
-
 const foodCategories = [
 
     spicy = [
-        {
-            name: 'Mexican',
-            foods: mexicanFoods
-        },
+        mexican = [
+            heavy = ['Burrito', 'Tacos', 'Enchiladas'],
+            light = ['Nachos', 'Quesadilla', 'Fajitas']
+        ],
 
-        {
-            name: 'Indian',
-            foods: indianFoods
-        },
+        indian = [
+            heavy = ['butter-chicken', 'chicken-masala', 'tikka', 'biryani', 'pakora'],
+            light = ['naan', 'samosa']
+        ],
 
-        {
-            name: 'Thai',
-            foods: thaiFoods
-        },
+        thai = [
+            heavy = ['pad-thai', 'khao-pad', 'pad-kra-pao-moo'],
+            light = ['panang-curry', 'tom-yum-goong', 'tom-kha-gai']
+        ],
     ],
 
     homestyle = [
-        {
-            name: 'American',
-            foods: americanFoods
-        },
+        american = [
+            heavy = ['burgers', 'steak', 'wings', 'fried-chicken', 'french-fries'],
+            light = ['salad', 'hot-dog']
+        ],
 
-        {
-            name: 'Italian',
-            foods: italianFoods
-        },
+        italian = [
+            heavy = ['spaghetti', 'pizza', 'lasagna', 'chicken-parm'],
+            light = ['bruscetta', 'risotto']
+        ],
 
-        {
-            name: 'Jewish',
-            foods: jewishFoods
-        },
+        jewish = [
+            heavy = ['knish', 'deli-sammy', 'challah', 'latkes'],
+            light = ['matzo-ball-soup', 'creamcheese-lox']
+        ],
     ],
 
     asian = [
-        {
-            name: 'Chinese',
-            foods: chineseFoods
-        },
+        chinese = [
+            heavy = ['orange-chicken', 'mongolian-beef', 'kung-pao-shrimp'],
+            light = ['eggroll', 'lo-mein', 'fried-rice']
+        ],
 
-        {
-            name: 'Japanese',
-            foods: japaneseFoods
-        },
+        japanese = [
+            heavy = ['ramen', 'teriyaki', 'tempura', 'katsudon'],
+            light = ['sushi', 'takoyaki']
+        ],
 
-        {
-            name: 'Vietnamese',
-            foods: vietnameseFoods
-        },
+        vietnamese = [
+            heavy = ['bahn-mi', 'com-tam', 'bun-cha', 'goi-cuon'],
+            light = ['spring-roll', 'pho']
+        ],
     ],
 
     sweet = [
-        {
-            name: 'Ice Cream',
-            foods: iceCreamFoods
-        },
+        iceCream = [
+            heavy = ['ice-cream-cake', 'banana-split', 'milkshake'],
+            light = ['ice-cream-cone', 'sundae', 'mochi']
+        ],
 
-        {
-            name: 'Bakery',
-            foods: bakeryFoods
-        },
+        bakery = [
+            heavy = ['cake', 'pie', 'brownies'],
+            light = ['crossiant', 'cookies', 'donut']
+        ],
 
-        {
-            name: 'French',
-            foods: frenchFoods
-        },
+        french = [
+            heavy = ["cake"],
+            light = ["croissant"]
+        ],
     ]
 
 ]
 
-var userSelection = []
 
-console.log(foodCategories[0][0].name)
+var userSelection = []
 
 /* Quiz Functions */
 
@@ -196,137 +135,156 @@ function nextQuestion(element) {
         answer2El.textContent = questionSet[2];
         answer3El.textContent = questionSet[3];
         answer4El.textContent = questionSet[4];
-        if (questionSelector === 1){answer4El.setAttribute("style", "display: none;")};
-        if (questionSelector === 2){answer3El.setAttribute("style", "display: none;")};
+        if (questionSelector === 1) { answer4El.setAttribute("style", "display: none;") };
+        if (questionSelector === 2) { answer3El.setAttribute("style", "display: none;") };
 
-            console.log(questionSet)
-            console.log(questionSelector)
-        }
-        else {
-            endQuiz(element);
-        }
+        console.log(questionSet)
+        console.log(questionSelector)
     }
-
-    function displayFinalChoice() {
-        var userSelection = JSON.parse(localStorage.getItem("userSelection"))
-        quizHeaderEl.textContent = userSelection[0].userChoice + userSelection[1].userChoice + userSelection[2].userChoice
+    else {
+        endQuiz(element);
     }
+}
 
-    function endQuiz(element) {
-        console.log(element.textContent)
-        console.log(localStorage.getItem("userSelection"))
-        displayFinalChoice();
-        quizEl.setAttribute("style", "height: 600px");
-        answer1El.setAttribute("style", "display: none;");
-        answer2El.setAttribute("style", "display: none;");
-        answer3El.setAttribute("style", "display: none;");
-        answer4El.setAttribute("style", "display: none;");
-        console.log(foodCategories);
-        console.log(groupID, "groupID");
-        console.log(weightID, "weightID");
-        // quizHeaderEl.textContent = foodCategories[foodCategory][groupID][weightID][0]
+
+function choiceDecipher() {
+    var userSelection = JSON.parse(localStorage.getItem("userSelection"))
+    var choices = [userSelection[0].userChoice, userSelection[1].userChoice, userSelection[2].userChoice]
+    console.log(choices[0]+ choices[1]+ choices[2])
+    console.log(groupID)
+  
+    if (choices[0] === "Spicy"){
+      foodCategory = 0
+    } if (choices[0] === "Homestyle"){
+      foodCategory = 1
+    } if (choices[0] === "Asian"){
+      foodCategory = 2
+    } if (choices[0] === "Sweet") {
+      foodCategory = 3
+    } if (choices[1] === "Mexican" ||choices[1] === "American" ||choices[1] === "Chinese" ||choices[1] === "Ice Cream") {
+      groupID = 0
+    } if (choices[1] === "Indian" ||choices[1] === "Italian" ||choices[1] === "Japanese" ||choices[1] === "Bakery") {
+      groupID = 1
+    } if (choices[1] === "Thai" ||choices[1] === "Jewish" ||choices[1] === "Vietnamese" ||choices[1] === "French") {
+      groupID = 2
+    } if (choices[2] === "Light") {
+      weightID = 1
     }
+    var finalChoice = foodCategories[foodCategory][groupID][weightID][0];
+    quizHeaderEl.textContent = finalChoice
+  }
 
-    startQuizEl.addEventListener("click", function (event) {
-        var element = event.target;
+function endQuiz(element) {
+    console.log(element.textContent)
+    console.log(localStorage.getItem("userSelection"))
+    choiceDecipher();
+    quizEl.setAttribute("style", "height: 600px");
+    answer1El.setAttribute("style", "display: none;");
+    answer2El.setAttribute("style", "display: none;");
+    answer3El.setAttribute("style", "display: none;");
+    answer4El.setAttribute("style", "display: none;");
+}
 
-        if (element.matches("#start-quiz")) {
-            var state = element.getAttribute("data-state");
+startQuizEl.addEventListener("click", function (event) {
+    var element = event.target;
 
-            if (state === "visible") {
-                element.dataset.state = "hidden";
-                element.setAttribute("style", "display: none;");
+    if (element.matches("#start-quiz")) {
+        var state = element.getAttribute("data-state");
 
-            };
-
-            quizEl.setAttribute("style", "height: 600px");
-            answer1El.setAttribute("style", "display: block;");
-            answer2El.setAttribute("style", "display: block;");
-            answer3El.setAttribute("style", "display: block;");
-            answer4El.setAttribute("style", "display: block;");
-            startQuiz();
-
+        if (state === "visible") {
+            element.dataset.state = "hidden";
+            element.setAttribute("style", "display: none;");
 
         };
 
-    });
+        quizEl.setAttribute("style", "height: 600px");
+        answer1El.setAttribute("style", "display: block;");
+        answer2El.setAttribute("style", "display: block;");
+        answer3El.setAttribute("style", "display: block;");
+        answer4El.setAttribute("style", "display: block;");
+        startQuiz();
 
-    function saveUserChoice(element) {
-        userSelection.push({
-            question: questionSet[questionSelector],
-            userChoice: element.textContent
-        })
-        localStorage.setItem("userSelection", JSON.stringify(userSelection))
+
+    };
+
+});
+
+function saveUserChoice(element) {
+    userSelection.push({
+        question: questionSet[questionSelector],
+        userChoice: element.textContent
+    })
+    localStorage.setItem("userSelection", JSON.stringify(userSelection))
+}
+
+answer1El.addEventListener("click", function (event) {
+    var element = event.target;
+
+    saveUserChoice(element);
+
+    if (element.textContent === "Spicy") {
+        optionsGroup = 1
+    } else {
+        optionsGroup = 5
+    }
+    console.log(optionsGroup)
+    nextQuestion(element)
+})
+
+answer2El.addEventListener("click", function (event) {
+    var element = event.target;
+
+    saveUserChoice(element);
+
+    if (element.textContent === "Homestyle") {
+        optionsGroup = 2
+    } else {
+        optionsGroup = 5
+    }
+    console.log(optionsGroup)
+    nextQuestion(element)
+})
+
+answer3El.addEventListener("click", function (event) {
+    var element = event.target;
+
+    saveUserChoice(element);
+
+    if (element.textContent === "Asian") {
+        optionsGroup = 3
+    } else {
+        optionsGroup = 5
     }
 
-    answer1El.addEventListener("click", function (event) {
-        var element = event.target;
+    console.log(optionsGroup)
+    nextQuestion(element)
+})
 
-        saveUserChoice(element);
+answer4El.addEventListener("click", function (event) {
+    var element = event.target;
 
-        if (element.textContent === "Spicy") {
-            optionsGroup = 1
-        } else { 
-            optionsGroup = 5 
-        }
-        console.log(optionsGroup)
-        nextQuestion(element)
-    })
+    saveUserChoice(element);
 
-    answer2El.addEventListener("click", function (event) {
-        var element = event.target;
-
-        saveUserChoice(element);
-
-        if (element.textContent === "Homestyle") {
-            optionsGroup = 2
-        } else {
-            optionsGroup = 5
-        }
-        console.log(optionsGroup)
-        nextQuestion(element)
-    })
-
-    answer3El.addEventListener("click", function (event) {
-        var element = event.target;
-
-        saveUserChoice(element);
-
-        if (element.textContent === "Asian") {
-            optionsGroup = 3
-        } else {
-            optionsGroup = 5
-        }
-
-        console.log(optionsGroup)
-        nextQuestion(element)
-    })
-
-    answer4El.addEventListener("click", function (event) {
-        var element = event.target;
-
-        saveUserChoice(element);
-
-        if (element.textContent === "Sweet") {
-            optionsGroup = 4
-        }
+    if (element.textContent === "Sweet") {
+        optionsGroup = 4
+    }
 
 
-        nextQuestion()
-    })
+    nextQuestion()
+})
 
 
-    var quickBtn = document.getElementById("quick-pick-btn");
-    var quickReturn = document.getElementById("quick-pick-return");
-    var foodsArray = ['taco', 'burrito', 'nachos', 'fajitas', 'quesadilla', 'enchilada', 'butter-chicken', 'chicken-masala', 'samosa', 'naan', 'tikka', 'biryani', 'pakora',
-        'tom-kha-gai', 'pad-thai', 'khao-pad', 'pad-kra-pao-moo', 'tom-yum-goong', 'panang-curry', 'burgers', 'hot-dog', 'steak', 'wings', 'fried-chicken', 'french-fries', 'salad',
-        'spaghetti', 'pizza', 'lasagna', 'bruscetta', 'chicken-parm', 'risotto', 'matzo-ball-soup', 'knish', 'creamcheese-lox', 'deli-sammy', 'challah', 'latkes',
-        'orange-chicken', 'mongolian-beef', 'lo-mein', 'fried-rice', 'kung-pao-shrimp', 'eggroll', 'sushi', 'ramen', 'teriyaki', 'tempura', 'katsudon', 'takoyaki',
-        'spring-roll', 'pho', 'bahn-mi', 'com-tam', 'bun-cha', 'goi-cuon', 'ice-cream-cone', 'ice-cream-cake', 'sundae', 'banana-split', 'milkshake', 'mochi',
-        'cake', 'donut', 'cookies', 'crossiant', 'pie', 'brownies', 'crepe', 'tarte-tatin', 'mille-fueille', 'macarons', 'creme-brulee', 'palmiers', 'madeleines'];
+var quickBtn = document.getElementById("quick-pick-btn");
+var quickReturn = document.getElementById("quick-pick-return");
+var foodsArray = ['taco', 'burrito', 'nachos', 'fajitas', 'quesadilla', 'enchilada', 'butter-chicken', 'chicken-masala', 'samosa', 'naan', 'tikka', 'biryani', 'pakora',
+    'tom-kha-gai', 'pad-thai', 'khao-pad', 'pad-kra-pao-moo', 'tom-yum-goong', 'panang-curry', 'burgers', 'hot-dog', 'steak', 'wings', 'fried-chicken', 'french-fries', 'salad',
+    'spaghetti', 'pizza', 'lasagna', 'bruscetta', 'chicken-parm', 'risotto', 'matzo-ball-soup', 'knish', 'creamcheese-lox', 'deli-sammy', 'challah', 'latkes',
+    'orange-chicken', 'mongolian-beef', 'lo-mein', 'fried-rice', 'kung-pao-shrimp', 'eggroll', 'sushi', 'ramen', 'teriyaki', 'tempura', 'katsudon', 'takoyaki',
+    'spring-roll', 'pho', 'bahn-mi', 'com-tam', 'bun-cha', 'goi-cuon', 'ice-cream-cone', 'ice-cream-cake', 'sundae', 'banana-split', 'milkshake', 'mochi',
+    'cake', 'donut', 'cookies', 'crossiant', 'pie', 'brownies', 'crepe', 'tarte-tatin', 'mille-fueille', 'macarons', 'creme-brulee', 'palmiers', 'madeleines'];
 
-    quickBtn.addEventListener("click", function () {
-        var random = (foodsArray[Math.floor(Math.random() * foodsArray.length)]);
-        quickReturn.innerHTML = random;
-        // console.log(random);
-    });
+quickBtn.addEventListener("click", function () {
+    var random = (foodsArray[Math.floor(Math.random() * foodsArray.length)]);
+    quickReturn.innerHTML = random;
+    // console.log(random);
+});
